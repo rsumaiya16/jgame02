3b01a56ceefc25792d43b6500aafbd4ac9182866
rsumaiya16 <rsumaiya1602@gmail.com>
2025-03-30 04:56:48 +0600

    2nd commit

---
+++ src/entity/Player.java
@@ -0,0 +1,94 @@
+package entity;
+
+import main.Gamepanel;
+import main.Keyhandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity {
+    Gamepanel gp;
+    Keyhandler keyH;
+
+      public Player(Gamepanel gp,Keyhandler keyH){
+          this.gp=gp;
+          this.keyH=keyH;
+
+          setDefaultValues();
+          getPlayerImage();
+
+      }
+      public void setDefaultValues(){
+          x=100;
+          y=100;
+          speed=2;
+          direction="down";
+      }
+      public void getPlayerImage(){
+          try{
+              up1= ImageIO.read(getClass().getResourceAsStream("playerup1.png"));
+              up2= ImageIO.read(getClass().getResourceAsStream("playerup2.png"));
+              down1= ImageIO.read(getClass().getResourceAsStream("playerdown1.png"));
+              down2= ImageIO.read(getClass().getResourceAsStream("playerdown2.png"));
+              right1= ImageIO.read(getClass().getResourceAsStream("playerright1.png"));
+              right2= ImageIO.read(getClass().getResourceAsStream("playerright2.png"));
+              left1= ImageIO.read(getClass().getResourceAsStream("playerleft1.png"));
+              left2= ImageIO.read(getClass().getResourceAsStream("playerleft2.png"));
+
+
+
+          }
+          catch(IOException e){
+              e.printStackTrace();
+
+          }
+      }
+      public void update(){
+          if(keyH.upPressed==true){
+              direction="up";
+              y-=speed;
+
+
+          }
+          else if(keyH.downPressed==true){
+              y+=speed;
+              direction="down";
+
+
+          }
+          else if(keyH.leftPressed==true){
+              x-=speed;
+              direction="left";
+          }
+          else if(keyH.rightPressed==true){
+              x+=speed;
+              direction="right";
+          }
+      }
+      public void draw(Graphics2D g2){
+
+          BufferedImage image=null;
+          switch (direction){
+              case "up":
+                  image=up1;
+                  break;
+              case "down":
+                  image=down1;
+                  break;
+              case "left":
+                  image=left1;
+                  break;
+              case "right":
+                  image=right1;
+                  break;
+
+
+          }
+          g2.drawImage(image,x,y,gp.tilesize, gp.tilesize, null);
+
+      }
+
+
+}
--- src/main/Gamepanel.java
+++ src/main/Gamepanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,18 +11,29 @@ public class Gamepanel extends JPanel implements Runnable {
     //screen settings
     final int originalTileSize=16;
     final int scale =3;
-    final int tilesize=originalTileSize*scale;
+    public final int tilesize=originalTileSize*scale;
     final int maxScreenCol=16;
     final int maxScreenRow=12;
     final int screenwidth=tilesize*maxScreenCol;
     final int screenheight=tilesize*maxScreenRow;
 
+    int fps=60;
+
+    Keyhandler keyH=new Keyhandler();
     Thread gameThread;
+    Player player=new Player(this,keyH);
+
+    //player's initial position
+    int playerX=100;
+    int playerY=100;
+    int playerspeed=2;
 
      public Gamepanel(){
          this.setPreferredSize(new Dimension(screenwidth,screenheight));
          this.setBackground(Color.black );
          this.setDoubleBuffered(true);
+         this.addKeyListener(keyH);
+         this.setFocusable(true);
      }
 
        public void startgamethread(){
@@ -31,26 +44,51 @@ public class Gamepanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+         double drawinterval=100000000/fps; //0.01666 sec
+        double nextdrawtime=System.nanoTime()+drawinterval;
+
+
+
          while(gameThread != null){
-             System.out.println("the game is running");
+
+
+             update();
+
+             repaint();
+
+
+
+             try {
+                 double remainingtime=nextdrawtime-System.nanoTime();
+                 remainingtime=remainingtime/100000;
+
+                 if(remainingtime<0){
+                     remainingtime=0;
+                 }
+                 Thread.sleep((long)remainingtime);
+
+                 nextdrawtime+=drawinterval;
+             } catch (InterruptedException e) {
+              e.printStackTrace();
+             }
+
+
          }
 
 
-         update();
 
-         repaint();
 
 
     }
     public void update(){
+         player.update();
 
     }
     public void paintComponent(Graphics g){
          super.paintComponent(g);
 
          Graphics2D g2=(Graphics2D) g;
-         g2.setColor(Color.white);
-         g2.fillRect(100,100,tilesize,tilesize);
+        player.draw(g2);
          g2.dispose();
 
 
